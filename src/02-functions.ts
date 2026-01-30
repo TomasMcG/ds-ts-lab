865aa6bcf3494f8358ca7d0002e51da945872604
tomas <tomasmcgrath21@gmail.com>
2026-01-30 09:16:39 +0000

    allOlder function

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,1 +1,27 @@
 import {Friend, Colleague } from './myTypes'
+import { friends,colleagues } from './01-basics'
+function older(f: Friend) : string {
+     f.age += 1
+     return `${f.name} is now ${f.age}` 
+}
+
+console.log(older(friends[0]))
+
+function allOlder(f: Friend[]): string[]  {
+    let olderFriends: string[] = []
+    f.forEach((friend)=>{olderFriends.push(older(friend))}) 
+    return olderFriends
+}
+
+console.log(allOlder(friends))
+
+
+// Find the colleague with the highest extension number.
+function highestExtension(cs: Colleague[]): Colleague {
+  const result = cs.sort(
+    (c1, c2) => c1.contact.extension - c2.contact.extension
+  );
+  return result[cs.length - 1];
+}
+console.log(highestExtension(colleagues.current));
+
