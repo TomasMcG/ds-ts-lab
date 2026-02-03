6a18080f9ab79ad9b39175d26c92c6d753dedff8
tomas <tomasmcgrath21@gmail.com>
2026-02-03 12:12:39 +0000

    Higher-order functions

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,4 +1,4 @@
-import {Friend, Colleague } from './myTypes'
+import {Friend, Colleague,EmailContact } from './myTypes'
 import { friends,colleagues } from './01-basics'
 function older(f: Friend)  {
      f.age += 1
@@ -35,3 +35,36 @@ function addColleague(clCur: Colleague[],clName: string, clDepartment: string, c
 
 addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
 console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
+
+
+
+
+function sortColleagues(
+  colleagues: Colleague[],
+  sorter: (c1: Colleague, c2: Colleague) => number
+): EmailContact[] {
+  const sorted = colleagues.sort(sorter); // Colleague[] inferred
+  const result: EmailContact[] = sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
+  return result 
+}
+
+console.log(sortColleagues(colleagues.current, (a, b) => a.contact.extension - b.contact.extension));
+console.log(sortColleagues(colleagues.current, (a, b) => a.name.length - b.name.length));
+
+
+
+
+
+
+
+
+function findFriends (fr: Friend[],searchCriteria: (friend:Friend) => boolean
+): Friend[] {
+return fr.filter(searchCriteria);
+
+}
+
+
+
+console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
+console.log(findFriends(friends, (friend) => friend.age < 35));
